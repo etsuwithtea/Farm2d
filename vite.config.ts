8cbc5143842f708901d083ff4eb9f724a523420e
etsuwithtea <navapan.suthon123@gmail.com>
2026-03-30 15:57:58 +0700

    ci: add GitHub Actions workflow for automated deployment to Pages and update base configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Farm2d/', // ตั้งค่า url base ให้ตรงกับชื่อ repository
   server: {
     port: 3000,
     open: false,
